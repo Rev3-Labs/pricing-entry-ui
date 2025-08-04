e0140c5b82772cff1949256390a7aaec909dd684
Josh Bauer <josh.bauer@rev3labs.com>
2025-08-04 18:54:51 -0400

    metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Pricing Entry",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: "Clean Earth Pricing",
+  description: "Clean Earth Pricing Entry System",
+  generator: "Clean Earth",
 };
 
 export default function RootLayout({
